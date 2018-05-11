1ccf8dca1b4773a0b04133e255b16b50a2fb19e3
Jesús Calderin <36571520+JCalderin91@users.noreply.github.com>
2018-05-11 14:58:11 -0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,19 +1,21 @@
-
-$(document).on('scroll', function(){
-	if($(document).scrollTop()>1){
-		//$('#navBar').addClass('sticky');
-		$('#btn-mas').fadeOut();
-		$('#posHeader').css('top','-40px');
-
-	}else{
-		//$('#navBar').removeClass('sticky');
-		$('#btn-mas').fadeIn();
-		$('#posHeader').css('top','0px');
-	}
+$(document).on('scroll', function() {
+    if ($(document).scrollTop() > 1) {
+        $('#navBar').addClass('sticky');
+        $('#btn-mas').fadeOut();
+        $('#btn-arriba').fadeIn();
+    } else {
+        $('#navBar').removeClass('sticky');
+        $('#btn-mas').fadeIn();
+        $('#btn-arriba').fadeOut();
+    }
+});
+$('#btn-arriba').on('click', function(e){
+	e.preventDefault();
+    $('html,body').animate({ scrollTop: 0 }, 1000);
 });
 
-$('.ancla').on('click', function(e){
-e.preventDefault();
-var strAncla = '#' + $(this).data('ancla');
-$('html,body').animate({scrollTop: $(strAncla).offset().top}, 1500);
+$('.ancla').on('click', function(e) {
+    e.preventDefault();
+    var strAncla = '#' + $(this).data('ancla');
+    $('html,body').animate({ scrollTop: $(strAncla).offset().top - 50 }, 1000);
 });
