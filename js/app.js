7984a52e40c9f734193ba388274636280cdfb973
Jesús Calderin <36571520+JCalderin91@users.noreply.github.com>
2018-05-11 03:08:46 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -12,5 +12,8 @@ $(document).on('scroll', function(){
 	}
 });
 
-
-
+$('.ancla').on('click', function(e){
+e.preventDefault();
+var strAncla = '#' + $(this).data('ancla');
+$('html,body').animate({scrollTop: $(strAncla).offset().top}, 1500);
+});
